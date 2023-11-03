bb5216612fbc429ef50626029fdef90e9e42fad8
alfarasjb <72119101+alfarasjb@users.noreply.github.com>
2023-11-03 15:11:51 +0800

    threading fixed

--- project.py
+++ project.py
@@ -1,19 +1,35 @@
-import customtkinter as ctk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+'''
+TODOS
+PENDING: 
+- 3 min video demo
+
+DONE:
+- main function DONE
+- 3 or more additional functions, and must be accompanied with pytest DONE
+- mainfile: project.py DONE
+- additional functions must be standalone (same level as main) DONE
+- test: test_project.py DONE
+- pip installable libraries in a file called "requirements.txt" DONE
+'''
 import warnings
-from fpdf import FPDF
 import os
 from datetime import datetime as dt
+from threading import Thread
 
-## stats
+from fpdf import FPDF
+import customtkinter as ctk
+from PIL import ImageTk
 import pandas as pd
 import numpy as np
 import scipy.stats as sc
 import yfinance as yf
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates 
 import math 
 import seaborn as sns
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates 
 
 
 warnings.filterwarnings('ignore')
@@ -36,6 +52,8 @@ class App(ctk.CTk):
         
         # WINDOW CONFIG
         self.title(title)
+        self.wm_iconbitmap()
+        self.iconphoto(True, ImageTk.PhotoImage(file = r'icon.png'))
         self.geometry(f'{w}x{h}')
         self.maxsize(w, h)
         self.minsize(w, h)
@@ -47,7 +65,10 @@ class App(ctk.CTk):
         self.build_stats()
         self.build_tabview()
 
-        
+        self.message = None
+        self.is_loading = False
+
+    ### ========== UI METHODS ========== ###
     def build_parent_frames(self):
         # header frame
         self.header_frame = ctk.CTkFrame(self)
@@ -60,31 +81,15 @@ class App(ctk.CTk):
         # plot frame
         self.plot_frame = ctk.CTkFrame(self)
         self.plot_frame.place(relx = 0.34, rely = 0.13, relwidth = 0.64, relheight = 0.85)
-
-    def build_tabview(self):
-        # tabview
-        tab_names = ['Histogram', 'Price']
-
-        self.tabview = ctk.CTkTabview(self.plot_frame, command = self.tab_func)
-        self.tabview.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
-        [self.tabview.add(tab_name) for tab_name in tab_names]
-
-    def tab_func(self):
-        
-        name = self.tabview.get()
-        master = self.tabview.tab(name)
-        
-        if self.cv is not None:
-            self.cv.pack_forget()
-        
-        if name == 'Histogram':
-            self.plot_fig(master, self.hist)
     
-        elif name == 'Price':
-            self.plot_fig(master, self.pc)
-    
+
+    def build_main_header(self):
+        # main header
+        self.main_header = ctk.CTkLabel(self.header_frame,
+                        text = title, font = ctk.CTkFont(size = 20, weight = 'bold')).pack(expand = True)
+
+
     def build_input_fields(self):
-
         # input fields: symbol and samples
         self.input_field_frame = ctk.CTkFrame(self.data_frame)
         self.input_field_frame.place(relx = 0, rely = 0, relwidth = 1, relheight = 0.33)
@@ -96,32 +101,23 @@ class App(ctk.CTk):
         self.sample_size = ctk.CTkLabel(master, text = 'Samples')
         self.sample_entry = ctk.CTkEntry(master, placeholder_text = 'Days')
         self.fetch_button = ctk.CTkButton(master, text = 'Fetch', command = self.fetch_data, width = 220)
-        self.message = ctk.CTkLabel(master, text = '')
 
         self.symbol_name.grid(row = 0, column = 0, padx = (20, 5), pady = (15, 5))
         self.sample_size.grid(row = 1, column = 0, padx = (20, 5), pady = 5)
         self.symbol_entry.grid(row = 0, column = 1, padx = (5, 10), pady = (15, 5))
         self.sample_entry.grid(row = 1, column = 1, padx = (5, 10), pady = 5)
         self.fetch_button.grid(row = 2, column = 0, columnspan = 2, padx = 10, pady = 5)
-        self.message.grid(row = 3, column = 0, columnspan = 2, padx = 10, pady = 0)
-        
-    def export_data(self):
-        try:
-            export(self.symbol, self.data, (self.hist, self.pc))
-        except AttributeError:
-            self.err_msg('Nothing to export')
+
 
     def build_stats(self, data:dict = {}):
         # ticker statistics table
         f_y = 0.35
         f_hgt = 1 - f_y
-
         self.stats_frame = ctk.CTkFrame(self.data_frame)
         self.stats_frame.place(relx = 0, rely = f_y, relwidth = 1, relheight = f_hgt)
 
-        index = ['Samples', 'Start Date', 'End Date', 'Mean ($)', 'Median ($)', 'Mode ($)', 'Std. Dev. ($)', 'Variance ($)', 'Skewness', 'Kurtosis']
-
         # columns 
+        index = ['Samples', 'Start Date', 'End Date', 'Mean ($)', 'Median ($)', 'Mode ($)', 'Std. Dev. ($)', 'Variance ($)', 'Skewness', 'Kurtosis']
         for i, idx in enumerate(index):
             y = (i * 25) + 5
             self.index_label = ctk.CTkLabel(self.stats_frame, text = idx)
@@ -134,33 +130,45 @@ class App(ctk.CTk):
         self.export_button = ctk.CTkButton(self.stats_frame, text = 'Export', command = self.export_data, width = 220)
         self.export_button.place(x = 10, rely = 0.87)
 
-        
-    def update_statistics(self, data:dict = {}):
-        for i, d in enumerate(data):
-            y = (i * 25) + 5
-            self.data_label = ctk.CTkLabel(self.stats_frame, text = data[d])
-            self.data_label.place(x = 130, y = y)
-  
-  
 
-    def build_main_header(self):
-        # main header
-        self.main_header = ctk.CTkLabel(self.header_frame,
-                        text = title, font = ctk.CTkFont(size = 20, weight = 'bold')).pack(expand = True)
-        
+    def build_tabview(self):
+        # tabview
+        tab_names = ['Histogram', 'Price']
 
-    def plot_fig(self, master, fig):
+        self.tabview = ctk.CTkTabview(self.plot_frame, command = self.tab_func)
+        self.tabview.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
+        [self.tabview.add(tab_name) for tab_name in tab_names]
 
-        # plot matplotlib figure
-        canvas = FigureCanvasTkAgg(fig, master = master)
-        canvas.draw()
+    ### ========== UI METHODS ========== ###
+
+    ### ========== UI COMMANDS ========== ###
+    def tab_func(self):
+        # parent: tabview
+        name = self.tabview.get()
+        master = self.tabview.tab(name)
         
-        self.cv = canvas.get_tk_widget()
-        self.cv.pack(expand = True, anchor = 's', padx = 5, pady = 5)
+        if self.cv is not None:
+            self.cv.pack_forget()
         
+        if name == 'Histogram':
+            self.plot_fig(master, self.hist)
     
+        elif name == 'Price':
+            self.plot_fig(master, self.pc)
+    
+
+    def export_data(self):
+        # parent: export
+        try:
+            fig_path = export(self.symbol, self.data, (self.hist, self.pc))
+            self.msg(f'Exported to: {fig_path}', 'confirm')
+        except AttributeError:
+            self.msg('Nothing to export')
+
+
     def fetch_data(self, symbol: str = '', samples: str = ''):
-        self.err_msg()
+        # parent: fetch
+        self.msg()
         self.data = None
         # validate entry, if pass, proceed to fetching data 
         try:
@@ -173,58 +181,108 @@ class App(ctk.CTk):
 
         except ValueError:
             # symbol input is empty
-            self.err_msg('Invalid Symbol Entry')
+            self.msg('Invalid Symbol Entry')
             return None
         except AssertionError:
             # samples input 
-            self.err_msg('Invalid Samples Entry')
+            self.msg('Invalid Samples Entry')
             return None
-
 
         try:
-            self.data, self.close = build_data(self.symbol, int(self.samples))
+            #self.data, self.close = build_data(self.symbol, int(self.samples))
+            # CREATE PROGRESS BAR HERE
+            self.msg(type = 'load')
+            start_download_thread(self, self.symbol, self.samples)
+            self.after(1000, self.check_if_done)
 
-            self.hist, self.pc = plot_data(self.close, self.data, self.symbol)
-            self.update_statistics(self.data)
-            self.tab_func()
+            
         except ValueError:
             # print error message on ui
-            self.err_msg(f'No data for symbol: {symbol}')
+            self.msg(f'No data for symbol: {symbol}')
             return None 
         except AssertionError:
-            self.err_msg(f'Invalid Samples')
+            self.msg(f'Invalid Samples')
             return None
         except AttributeError:
-            self.err_msg('No data')
+            self.msg('No data')
             return None
         
         return symbol, samples
-
-    def err_msg(self, err_msg: str = ''):
-
-        # error message 
-        self.message.configure(text = err_msg, text_color = 'red')
+    ### ========== UI COMMANDS ========== ###
 
 
+    ### ========== UI UPDATES / UTILITIES ========== ###
+    def update_statistics(self, data:dict = {}):
+        for i, d in enumerate(data):
+            y = (i * 25) + 5
+            self.data_label = ctk.CTkLabel(self.stats_frame, text = data[d])
+            self.data_label.place(x = 130, y = y)
+  
 
-def main():
-    app = App()
-    app.mainloop()
+    def plot_fig(self, master, fig):
 
+        # plot matplotlib figure
+        canvas = FigureCanvasTkAgg(fig, master = master)
+        canvas.draw()
+        
+        self.cv = canvas.get_tk_widget()
+        self.cv.pack(expand = True, anchor = 's', padx = 5, pady = 5)
+          
+
+    def check_if_done(self):
+        if not self.is_loading:
+            # END PROGRESS BAR HERE
+            self.message.destroy()
+            try:
+                self.hist, self.pc = plot_data(self.close, self.data, self.symbol)
+                self.update_statistics(self.data)
+                self.tab_func()
+            except AttributeError:
+                self.msg(f'No data for symbol: {self.symbol}')
+                return None
+        else:
+            self.after(1000,self.check_if_done)
+
+
+    def msg(self, err_msg: str = '', type:str = 'err'):
+        master = self.input_field_frame
+        if self.message is not None:
+            self.message.destroy()
+        
+        if type == 'load':
+            self.message = ctk.CTkProgressBar(master, orientation = 'horizontal', mode = 'indeterminate')
+            self.message.grid(row = 3, column = 0, columnspan = 2, padx = 10, pady = (10,0))   
+            self.message.start()
+
+        else:
+            # error message
+            col = 'red' if type == 'err' else 'green'
+            self.message = ctk.CTkLabel(master, text = err_msg, text_color=col)
+            self.message.grid(row = 3, column = 0, columnspan = 2, padx = 10, pady = 0)
+            
+            # loading progress bar
+            
+
+
+    ### ========== UI UPDATES / UTILITIES ========== ###
+
+
+
+### ========== GENERAL UTILITIES / VALIDATION ========== ###
 def validate_entry(symbol:str = '', samples:str = ''):
         '''
         testing: symbol and samples == '' raises value error
         letters in samples raises assertion error
         '''
         # get entry field values
-
         # compare: (func parameters are only used for unit testing)
         # use func parameters if input fields are empty
 
         # main validation: 
         # symbol = valid string
         # sample is numeric
-        
+
+
         # check if fields are not empty
         # leave this portion below assignment, otherwise, testing will be faulty
         if symbol == '' or samples == '':
@@ -235,9 +293,15 @@ def validate_entry(symbol:str = '', samples:str = ''):
 
         return symbol, int(samples)
 
+def start_download_thread(app, symbol:str = 'AAPL', samples:int = 10):
+    b = Thread(target = build_data, args = [app, symbol, samples])
+    b.start()
+    app.is_loading = True
+
+### ========== GENERAL UTILITIES / VALIDATION ========== ###
 
 
-
+### ========== CRITICAL METHODS ========= ###
 def download(symbol:str = 'AAPL', samples:int = 10):
     '''
     # testing: samples assertion 
@@ -245,22 +309,25 @@ def download(symbol:str = 'AAPL', samples:int = 10):
     # output shape (samples, columns (6))
     # test wrong symbol raises value error
     '''
-    print('downloading')
-    
     df = yf.download(symbol, interval = '1d').tail(samples)
+
     if df.empty:
         raise ValueError(f'No data for symbol: {symbol}')
-    
+
     return df
 
-def build_data(symbol:str = 'AAPL', samples:int = 10):
+def build_data(app, symbol:str = 'AAPL', samples:int = 10):
     '''
     testing: samples assertion
     output type: list, pd.series
     lower case symbol
     '''
     assert samples > 5
-    df = download(symbol.upper(), samples)
+    try:
+        df = download(str(symbol).upper(), samples)
+    except ValueError: 
+        app.is_loading = False # triggers end of download
+        return None # ends thread
     close = df['Close']
 
     mean, median, mode = np.mean(close), np.median(close), sc.mode(close, keepdims = True)
@@ -272,6 +339,9 @@ def build_data(symbol:str = 'AAPL', samples:int = 10):
     key = ['samples','start','end','mean','median','mode','sdev','var','skew','kurt']
     data = {k:v for k,v in zip(key,updated)}
 
+    app.data = data
+    app.close = close
+    app.is_loading = False
 
     return data, close
 
@@ -366,21 +436,17 @@ def export(sym: str, main_data: dict, figs:tuple = None):
 
     pdf.output(pdf_path)
 
+    return f'{sym}_{smpl}_{today}'
+### ========== CRITICAL METHODS ========== ###
 
 
-    
-'''
-PROJECT TODOS
-- main function DONE
-- 3 or more additional functions, and must be accompanied with pytest DONE
-- mainfile: project.py DONE
-- additional functions must be standalone (same level as main) DONE
-- test: test_project.py DONE
-- pip installable libraries in a file called "requirements.txt"
 
-'''
-
-
+### ========== MAIN ========== ###
+def main():
+    app = App()
+    app.mainloop()
 
 if __name__ == "__main__":
-    main()
+    t = Thread(target = main)
+    t.start()
+### ========== MAIN ========== ###
